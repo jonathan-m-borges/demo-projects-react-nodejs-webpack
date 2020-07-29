c2b7580db502d5a1dec72a986f89fe888e1019dd
Jonathan M Borges <jonathan.m.borges@gmail.com>
2020-07-29 01:09:19 -0300

    Adicionando CORS no backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { uuid, isUuid } = require('uuidv4');
 const config = require('./config');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const projects = [];
